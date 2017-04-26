356ffcf30273e086c6188e517f34a51ac0a92198
Santiago de Diego <santidediego@hotmail.com>
2017-04-26 12:38:23 +0200

    Colored metadata

--- modules/pdf_metadata.py
+++ modules/pdf_metadata.py
@@ -6,15 +6,15 @@ def metadata():
     try:
         pdfFile=PdfFileReader(open(path,'rb'))
     except:
-        print("\nNo pdf was found")
+        print(colored.red("\nNo pdf was found"))
         return
     metadatas=pdfFile.getDocumentInfo()
-    print("\nMetadatas:")
+    print(colored.cyan("\nMetadatas:"))
     print("----------------------------")
     #metadatas is none, check first
     if(metadatas is not None):
         for data in metadatas:
-            print("[+]"+data+ ":"+metadatas[data])
+            print(colored.green("[+]"+data+ ":"+metadatas[data]))
 
     print (colored.yellow("\n<Enter>\n"))
     input()
